75197722418f77298f40c3759f54cf2d5f685f5b
whatisyourname0 <a01032208149@gmail.com>
2023-02-20 22:10:33 +0900

    Build : Configure SWC

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,45 @@
 const { CracoAliasPlugin } = require("react-app-alias");
+const CracoSwcPlugin = require("craco-swc");
 
 module.exports = {
-  esline: {
-    enable: false,
-  },
   plugins: [
+    {
+      plugin: {
+        ...CracoSwcPlugin,
+        overrideCracoConfig: ({ cracoConfig }) => {
+          if (typeof cracoConfig.eslint.enable !== "undefined") {
+            cracoConfig.disableEslint = !cracoConfig.eslint.enable;
+          }
+          delete cracoConfig.eslint;
+          return cracoConfig;
+        },
+        overrideWebpackConfig: ({ webpackConfig, cracoConfig }) => {
+          if (
+            typeof cracoConfig.disableEslint !== "undefined" &&
+            cracoConfig.disableEslint === true
+          ) {
+            webpackConfig.plugins = webpackConfig.plugins.filter(
+              (instance) => instance.constructor.name !== "ESLintWebpackPlugin"
+            );
+          }
+          return webpackConfig;
+        },
+      },
+      options: {
+        swcLoaderOptions: {
+          jsc: {
+            externalHelpers: true,
+            target: "es5",
+            parser: {
+              syntax: "ecmascript",
+              jsx: true,
+              dynamicImport: true,
+              exportDefaultFrom: true,
+            },
+          },
+        },
+      },
+    },
     {
       plugin: CracoAliasPlugin,
       options: {},
