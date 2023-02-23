a761346cd62a6981f745411ea678960dc11f47ca
whatisyourname0 <a01032208149@gmail.com>
2023-02-24 00:33:48 +0900

    Test skewness on gh-pages

--- src/pages/Clubs/GalleryComponent/GalleryItem/index.jsx
+++ src/pages/Clubs/GalleryComponent/GalleryItem/index.jsx
@@ -10,13 +10,16 @@ function GalleryItem({ index, desc, short }) {
   const [skew, setSkew] = useState(0);
 
   useEffect(() => {
-    scroll.on("scroll", (obj) => {
-      scrollPos.current = obj.scroll.x;
-      const distance = scrollPos.current - scrollPos.previous;
-      scrollPos.previous = scrollPos.current;
-      const skewness = mapNumber(distance, -100, 100, -10, 10);
-      setSkew(skewness);
-    });
+    if (scroll) {
+      scroll.on("scroll", (obj) => {
+        scrollPos.current = obj.scroll.x;
+        const distance = scrollPos.current - scrollPos.previous;
+        scrollPos.previous = scrollPos.current;
+        const skewness = mapNumber(distance, -100, 100, -10, 10);
+        setSkew(skewness);
+        console.log(scrollPos, skewness);
+      });
+    }
   }, [scroll]);
 
   return (
