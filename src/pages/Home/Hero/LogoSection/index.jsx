eb53fae269a47f488c6578b430550501e26504e4
whatisyourname0 <a01032208149@gmail.com>
2023-04-07 00:08:08 +0900

    Fix : fix initial opacity load

--- src/pages/Home/Hero/LogoSection/index.jsx
+++ src/pages/Home/Hero/LogoSection/index.jsx
@@ -1,10 +1,20 @@
 import GDSCLogo from '@assets/images/gdsclogo.png';
+import { useState } from 'react';
 import styled from 'styled-components';
 
 function LogoSection() {
+  const [opacity, setOpacity] = useState(0);
+
   return (
     <Container data-scroll data-scroll-position="top" data-scroll-speed="2">
-      <LogoImg src={GDSCLogo} alt="GDSCLogo" />
+      <LogoImg
+        src={GDSCLogo}
+        alt="GDSCLogo"
+        opacity={opacity}
+        onLoad={() => {
+          setOpacity(1);
+        }}
+      />
     </Container>
   );
 }
@@ -20,7 +30,11 @@ const Container = styled.div`
   align-items: center;
 `;
 
-const LogoImg = styled.img`
+const LogoImg = styled.img.attrs((props) => ({
+  style: {
+    opacity: `${props.opacity}`,
+  },
+}))`
   margin-top: auto;
   width: min(30vw, 150px);
 
